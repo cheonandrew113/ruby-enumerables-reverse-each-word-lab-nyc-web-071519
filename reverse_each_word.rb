6d90fe8dc2414007758cd72bbe4781b56d356ea4
Andnrew Cheon <cheonandrew113@gmail.com>
2019-07-02 05:33:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,11 @@ def reverse_each_word(sentence1)
   new_sentence = new_words.join(" ")
   return new_sentence
 end
+
+def reverse_each_word(sentence1)
+  words = sentence1.split(" ")
+  new_words = []
+  words.collect { |word| new_words << word.reverse }
+  new_sentence = new_words.join(" ")
+  return new_sentence
+end
