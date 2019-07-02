100331e9f45e41780fb80bec491423b9872f30ca
Andnrew Cheon <cheonandrew113@gmail.com>
2019-07-02 05:30:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,8 @@
 def reverse_each_word(sentence1)
-  
+  words = sent.split(" ")
+  new_words = []
+  words.each { |word| new_words << word.reverse }
+  new_sent = new_words.join(" ")
+  return new_sent
   
 end
